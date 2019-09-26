cca4cdf22c936239bc8bf73626f4945f6d8f78ed
Kyle Fitzpatrick <kyle.shadow@outlook.com>
2019-09-26 19:34:19 +1000

    Basic Scripts
    Basic Player Movement (run into cube "trees" to reduce your health and increase Greenhouse Gas levels)

---
+++ Final Act/Assets/Scripts/CutDownTree.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutDownTree : MonoBehaviour
+{
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Tree"))
+        {
+            GetComponent<PlayerHealth>().health--;
+            GetComponent<GreenHouseGasLevels>().CO2Level++;
+        }
+    }
+}
